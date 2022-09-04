f329e1b0199af1b079fe571d9af067a49d8a4031
Krzysztof Krawczyk <krzysztofskrawczyk@gmail.com>
2022-09-04 11:44:24 +0200

    Added field

--- src/main/java/com/bots/RacoonShared/Discord/MessageLog.java
+++ src/main/java/com/bots/RacoonShared/Discord/MessageLog.java
@@ -6,14 +6,16 @@ public class MessageLog {
     public final String serverId, channelId;
     public final String username;
     public final Color userColor;
+    public final String message;
     private boolean userIsBot = false, hasEmbeds = false;
 
     public MessageLog(String serverId, String channelId,
-                      String username, Color userColor) {
+                      String username, Color userColor, String message) {
         this.serverId = serverId;
         this.channelId = channelId;
         this.username = username;
         this.userColor = userColor;
+        this.message = message;
     }
 
     public boolean userIsBot() {
