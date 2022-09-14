e4128772c0ce40e065f8d9abac75095e2c180252
Krzysztof Krawczyk <krzysztofskrawczyk@gmail.com>
2022-09-14 12:25:27 +0200

    Changed MessageLog to record

--- src/main/java/com/bots/RaccoonShared/Discord/MessageLog.java
+++ src/main/java/com/bots/RaccoonShared/Discord/MessageLog.java
@@ -3,37 +3,7 @@ package com.bots.RaccoonShared.Discord;
 import java.awt.*;
 import java.io.Serializable;
 
-public class MessageLog implements Serializable {
-    public final String serverId, channelId;
-    public final String username;
-    public final Color userColor;
-    public final String message;
-    private boolean userIsBot = false, hasEmbeds = false;
-
-    public MessageLog(String serverId, String channelId,
-                      String username, Color userColor, String message) {
-        this.serverId = serverId;
-        this.channelId = channelId;
-        this.username = username;
-        this.userColor = userColor;
-        this.message = message;
-    }
-
-    public boolean userIsBot() {
-        return userIsBot;
-    }
-
-    public MessageLog setUserIsBot(boolean userIsBot) {
-        this.userIsBot = userIsBot;
-        return this;
-    }
-
-    public boolean hasEmbeds() {
-        return hasEmbeds;
-    }
-
-    public MessageLog setHasEmbeds(boolean hasEmbeds) {
-        this.hasEmbeds = hasEmbeds;
-        return this;
-    }
+public record MessageLog(String serverId, String channelId, String username,
+                         Color userColor, String message, boolean userIsBot,
+                         boolean hasEmbeds) implements Serializable {
 }
