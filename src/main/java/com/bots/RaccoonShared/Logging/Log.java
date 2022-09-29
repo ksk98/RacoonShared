67bffcbc7df4e66897efbbda91dae91c339a776e
Krzysztof Krawczyk <krzysztofskrawczyk@gmail.com>
2022-09-29 09:11:03 +0200

    Added date and time to log

--- src/main/java/com/bots/RaccoonShared/Logging/Log.java
+++ src/main/java/com/bots/RaccoonShared/Logging/Log.java
@@ -2,15 +2,21 @@ package com.bots.RaccoonShared.Logging;
 
 import java.awt.*;
 import java.io.Serializable;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Log implements Serializable {
+    private static final DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
     private String caller, message;
     private Color color;
+    private Date date;
 
     public Log(String caller, String message, Color color) {
         this.caller = caller;
         this.message = message;
         this.color = color;
+        this.date = new Date();
     }
 
     public String getCaller() {
@@ -37,8 +43,16 @@ public class Log implements Serializable {
         this.color = color;
     }
 
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
     @Override
     public String toString() {
-        return caller + ": " + message;
+        return dateFormat.format(date) + " " + caller + ": " + message;
     }
 }
